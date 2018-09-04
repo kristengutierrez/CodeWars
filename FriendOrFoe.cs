2a5d6b1789a75baf1efd25a7d6c7b7b8a72ebdf4
Kristen Kozmary <kristen.kozmary@magnetek.com>
2018-09-04 08:18:57 -0500

    Friend or Foe kata

---
+++ FriendOrFoe.cs
@@ -0,0 +1,31 @@
+/*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+Note: keep the original order of the names in the output.
+*/
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class Kata {
+  public static IEnumerable<string> FriendOrFoe (string[] names) 
+  {
+    /* 
+    //OLD SOLUTION
+    foreach (string name in names)
+    {
+      if (name.Length == 4)
+      {
+        yield return name;
+      }
+    }
+    */
+    //NEW SOLUTION
+    IEnumerable<string> result = names.Where(name => name.Length == 4);
+    return result;
+  }
+}
